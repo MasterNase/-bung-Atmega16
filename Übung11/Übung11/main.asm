adc2d2c48bc7fc0e665328c19c30b1f0d2a645b4
MasterNase <wurz1@gmx.net>
2017-01-30 20:13:30 +0100

    Stuff

--- Übung11/Übung11/main.asm
+++ Übung11/Übung11/main.asm
@@ -26,6 +26,9 @@ SSEG_CODE:
 .endm
 
 .def work = r16
+.def work2 = r17
+.def count_low = r18
+.def count_high = r19
 
 INIT_PORTS:
 	PUSH work		;save work to stack
@@ -46,57 +49,114 @@ INIT_PORTS:
 	POP work
 	RET	
 
-SSEG_OUT:
-	push work
-	in work, PINC
-	cpi work, $7B
-	breq SSEG_OUT_NEXT
-	ld work, X+
-	cbi PORTA, 2
-	out PORTC, work
-	pop work
-	rjmp SSEG_OUT
-SSEG_OUT_NEXT:
-	ldi XH, high(Table)
-	ldi XL, low(Table)
-	ld work, Z+
-	cpi work, $5F
-	breq SSEG_OUT_RESET
-	out PORTB, work
-	clr work
-	out PORTC, work
-	pop work
-	rjmp SSEG_OUT
-SSEG_OUT_RESET:
-	clr work
-	out PORTB, work
-	out PORTC, work
+;SSEG_OUT:
+;	push work
+;	in work, PINC
+;	cpi work, $7B
+;	breq SSEG_OUT_NEXT
+;	ld work, X+
+;	cbi PORTA, 2
+;	out PORTC, work
+;	pop work
+;	rjmp SSEG_OUT
+;SSEG_OUT_NEXT:
+;	ldi XH, high(Table)
+;	ldi XL, low(Table)
+;	ld work, Z+
+;	cpi work, $5F
+;	breq SSEG_OUT_RESET
+;	out PORTB, work
+;	clr work
+;	out PORTC, work
+;	pop work
+;	rjmp SSEG_OUT
+;SSEG_OUT_RESET:
+;	clr work
+;	out PORTB, work
+;	out PORTC, work
+;	ldi ZH, high(Table)
+;	ldi ZL, low(Table)
+;	pop work
+;	rjmp SSEG_OUT
+
+SSEG_OUT:					;advances through the lookup table until the pointer is at the right
+	ldi ZH, high(Table)		;number and outputs it to portb or c
+	ldi ZL, low(Table)
+	mov work, count_low
+	cpi count_low, $00
+	breq skip
+Loop_low:					;goes through the list for the low number
+	adiw ZL, 1
+	dec work
+	brne Loop_low
+skip:						;skips the loop when count_low = 0
+	ld work, Z
+	out PORTC, work			;outputs the number found in the table to port c
+	ldi work2, $16
+	mov work, count_high
 	ldi ZH, high(Table)
 	ldi ZL, low(Table)
-	sbis PORTA, 0
-	sbi PORTA, 2
-	pop work
-	rjmp SSEG_OUT
-
-COUNT_UP:
+	cpi work, $00
+	brne Loop_high
+	ret
+Loop_high:					;goes through the table for the high number
+	adiw ZL, 1
+	subi work, $16
+	brne Loop_high
+	ld work,Z
+	out PORTB,work			;outputs the number found in the table to portb
+	ret
 	
+COUNT_UP:
+	push work				;increments count_low
+	push work2
+	cbi PORTA, 2
+	cpi count_low, $09
+	breq count_up_high
+	inc count_low
+	call SSEG_OUT
+	pop work
+	pop work2
+	ret
+count_up_high:
+	ldi count_low, $00		;increments count_high and resets count_low
+	cpi count_high, $50
+	breq count_reset
+	ldi work, $16
+	add count_high, work
+	call SSEG_OUT
+	pop work
+	pop work2
+	ret
+count_reset:				;resets the counter after 1 minute has passed	
+	ldi count_high, $00		
+	out PORTB, count_high	;sets portb back to 0
+	sbi PORTA, 2			;sets PINA2 to 1
+	call SSEG_OUT
+	pop work
+	pop work2
+	ret
 	
 ; Replace with your application code
 Main:
-	INITSP
-    call INIT_PORTS
-	call Initialize_SSeg
+	INITSP					;Initializes StackPointer
+    call INIT_PORTS			;initializes ports
+	call Initialize_SSeg	;initiializes the lookup-table
 Reset:	
-	ldi XH, high(Table)
-	ldi XL, low(Table)
-	ldi ZH, high(Table)
-	ldi ZL, low(Table)
-	ld work, Z+
+	ldi ZH, high(Table)		;sets the output to the initial 00
+	ldi ZL, low(Table)		
+	ld work, Z
 	out PORTB, work
-	ld work, X+
+	ld work, Z
 	out PORTC, work
+	ldi work, $F0
 Loop:	
-	sbic PINA, 0
+	sbrc work, 0
+	sbi PORTA, 3
+	sbrs work, 0
+	cbi PORTA, 3
+	swap work
+	sbic PINA, 0			;loops until input or while pina1 is set
 	rjmp Reset
 	sbic PINA, 1
 	rjmp LOOP
